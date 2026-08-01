cf67ded894d6e6e1e8afdf594c13a17de1fb40f2
Aman <aman.webdevlopment@gmail.com>
2026-08-01 12:00:13 +0530

    Prevent accidental Android app exits

--- android/app/src/main/java/com/fptrades/portal/MainActivity.java
+++ android/app/src/main/java/com/fptrades/portal/MainActivity.java
@@ -2,9 +2,12 @@ package com.fptrades.portal;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
+import android.widget.Toast;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private long lastExitAttemptAt = 0L;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +20,16 @@ public class MainActivity extends BridgeActivity {
             android.webkit.WebView.RENDERER_PRIORITY_IMPORTANT,
             false
         );
+    }
+
+    @Override
+    public void finish() {
+        long now = System.currentTimeMillis();
+        if (now - lastExitAttemptAt > 2000L) {
+            lastExitAttemptAt = now;
+            Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        super.finish();
     }
 }
